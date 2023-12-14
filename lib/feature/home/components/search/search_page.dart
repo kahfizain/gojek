4da3a3ac601ef78385f8d53cf357f15081b4f027
KAHFI ZAIN <zkahfi@gmail.com>
2023-12-14 11:13:50 +0700

    item history search

--- lib/feature/home/components/search/search_page.dart
+++ lib/feature/home/components/search/search_page.dart
@@ -11,40 +11,42 @@ class SearchHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: () {
-        Navigator.push(
-            context, MaterialPageRoute(builder: (context) => Search()));
-      },
+
       child: Padding(
         padding: const EdgeInsets.only(top: 23, left: 15, right: 15),
         child: Row(
           children: [
             Flexible(
                 fit: FlexFit.tight,
-                child: Container(
-                  padding: const EdgeInsets.all(8),
-                  decoration: BoxDecoration(
-                      color: const Color(0xFFFAFAFA),
-                      borderRadius: BorderRadius.circular(30),
-                      border: Border.all(color: const Color(0xFFE8E8E8))),
-                  child: Row(
-                    children: [
-                      SvgPicture.asset(
-                        'assets/icons/search.svg',
-                        color: dark1,
-                        width: 20,
-                        height: 20,
-                      ),
-                      const SizedBox(
-                        width: 10,
-                      ),
-                      Text(
-                        "Cari layanan, makan , & tujuan",
-                        style: regular14.copyWith(color: dark3),
-                      ),
-                    ],
-                  ),
-                )),
+                child: GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                          context, MaterialPageRoute(builder: (context) => Search()));
+                    },
+                  child: Container(
+                    padding: const EdgeInsets.all(8),
+                    decoration: BoxDecoration(
+                        color: const Color(0xFFFAFAFA),
+                        borderRadius: BorderRadius.circular(30),
+                        border: Border.all(color: const Color(0xFFE8E8E8))),
+                    child: Row(
+                      children: [
+                        SvgPicture.asset(
+                          'assets/icons/search.svg',
+                          color: dark1,
+                          width: 20,
+                          height: 20,
+                        ),
+                        const SizedBox(
+                          width: 10,
+                        ),
+                        Text(
+                          "Cari layanan, makan , & tujuan",
+                          style: regular14.copyWith(color: dark3),
+                        ),
+                      ],
+                    ),
+                  )),),
             const SizedBox(width: 15,),
             SizedBox(
               width: 35,
